a022c9515a2b5ddb0e99e4172566a783e953610c
vbjiny <vbjiny@gmail.com>
2022-05-18 15:58:59 +0900

    ㅁㅁ

--- src/main/java/com/example/demo/cmmn/dataaccess/util/ParamMap.java
+++ src/main/java/com/example/demo/cmmn/dataaccess/util/ParamMap.java
@@ -7,6 +7,11 @@ import org.springframework.jdbc.support.JdbcUtils;
 public class ParamMap extends ListOrderedMap<Object, Object> { 
 	// ... 
 	
+	/**
+	 * EgovMap 흉내내기
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Object put(Object key, Object value) { 
 		// StringUtils.lowerCase 로 key값을 소문자로 변경 (USER_NAME => user_name) 
 		// JdbcUtils.convertUnderscoreNameToPropertyName 로 key값을 camelCase로 변경 (user_name => userName) 
