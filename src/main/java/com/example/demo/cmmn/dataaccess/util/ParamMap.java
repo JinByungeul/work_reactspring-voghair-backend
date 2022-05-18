7806ab3469be116e7535915167bf6524ab28f12d
vbjiny <vbjiny@gmail.com>
2022-05-18 15:50:46 +0900

    aa

---
+++ src/main/java/com/example/demo/cmmn/dataaccess/util/ParamMap.java
@@ -0,0 +1,16 @@
+package com.example.demo.cmmn.dataaccess.util;
+
+import org.apache.commons.collections4.map.ListOrderedMap;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+public class ParamMap extends ListOrderedMap<Object, Object> { 
+	// ... 
+	
+	public Object put(Object key, Object value) { 
+		// StringUtils.lowerCase 로 key값을 소문자로 변경 (USER_NAME => user_name) 
+		// JdbcUtils.convertUnderscoreNameToPropertyName 로 key값을 camelCase로 변경 (user_name => userName) 
+		return super.put(JdbcUtils.convertUnderscoreNameToPropertyName(StringUtils.lowerCase((String) key)), value); 
+	
+	} 
+}
